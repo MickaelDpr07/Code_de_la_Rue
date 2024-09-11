b7f28bee225dc8710f36fe73b58cf848ab631a33
DeSstreK <101575701+DeSstreK@users.noreply.github.com>
2024-09-11 18:24:04 +0200

    Fin du prototype lexique
    Affichage du titre de catégorie et de la définition choisis !

--- Code_de_la_Rue/Assets/Devellopeur/Pierre/Script Test/Lexique/Lexique.cs
+++ Code_de_la_Rue/Assets/Devellopeur/Pierre/Script Test/Lexique/Lexique.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Lexique : MonoBehaviour
 {
     public GameObject PanelDescription;
     public GameObject PanelLexique;
     public GameObject PanelButton;
+    
 
+    // A Afficher
+    public TextMeshProUGUI Categorie;
     public TextMeshProUGUI titre;
     public TextMeshProUGUI contenu;
 
@@ -26,8 +31,45 @@ public class Lexique : MonoBehaviour
 
     public void OpenText(string description)
     {
-        PanelDescription.SetActive(true);
+        //Récupère l'objet dernièrement séléctionné est le stock dans la variable
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+
+        //CATEGORIE
+
+        Transform ancestor = clickedButton.transform;
+
+        //Récupère l'ancêtre 4 du boutton
+        for (int i = 0; i < 4; i++)
+        {
+            if (ancestor != null)
+            {
+                ancestor = ancestor.parent;
+        
+            }
+            else
+            {
+                Debug.LogError("Le parent à la hiérarchie demandée n'existe pas.");
+                return;
+            }
+        }
+        // Trouver le composant Text dans cet ancêtre
+        TextMeshProUGUI textComponent = ancestor.GetComponentInChildren<TextMeshProUGUI>();
+        //l'applique au titre de la definition
+        Categorie.text = textComponent.text;
+        
+
+        //TITRE
+
+        //Je récupère le textmeshpro de l'enfant du gameobject dernièrement séléctionner soit le boutton
+        TextMeshProUGUI buttonText = clickedButton.GetComponentInChildren<TextMeshProUGUI>();
+        titre.text = buttonText.text;
+
+        //CONTENUE
+
         contenu.text = description;
+
+
+        PanelDescription.SetActive(true);
 
     }
 
